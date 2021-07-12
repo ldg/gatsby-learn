17b4fbe3c4402fedaccc4aefba9db2b8ffac04ba
smldgweb <sales@ldgwebdesign.com>
2021-07-12 15:57:23 -0400

    Updated content and styles

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,63 @@
 import * as React from "react"
 
+/*Styles*/
+const AboutStyles = {
+	background: "#34343430",
+	padding: "1rem",
+	textAlign: "center",
+	margin: "auto",
+	width: "600px"
+}
+const ListSection = {
+	border: "1px solid #343434",
+	margin: "auto",
+	width: "632px"
+}
+const ItemStyles = {
+	listStyle: "none" 
+}
 
+/* Data */
+const items = [
+	{
+		text: "Yahoo News",
+		url: "https://news.yahoo.com/",
+		description: "A website with lots of news"
+	},
+	{
+		text: "To Many Kittens!",
+		url: "https://www.etsy.com/market/too_many_kittens",
+		description: "An etsy store with kitten based products :]"
+	},
+	{
+		text: "Steam Store",
+		url: "https://store.steampowered.com/",
+		description: "A place to buy video games :o"
+	}
+]
+
+/* Page */
 const AboutPage = () => {
 	return(
-		<div>
-			<h1>About Page</h1>
-			<p>Well this is akward</p>
-		</div>
-			
+		<section style={ListSection}>
+			<div style={AboutStyles}>
+				<h1>About Page</h1>
+				<p>Well this is akward</p>
+			</div>
+			<div>
+				<ul>
+				{items.map(item =>(
+					<li style={ItemStyles}>
+						<a href={item.url}>
+							{item.text}
+						</a>
+						<p>{item.description}</p>
+					</li>
+				))}
+					
+				</ul>
+			</div>
+		</section>		
 		)
 }
 
