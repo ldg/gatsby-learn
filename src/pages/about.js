7d5b8c9b664280c5fb6751272d2a47c7b7118fd6
smldgweb <sales@ldgwebdesign.com>
2021-07-12 14:50:54 -0400

    Adding an About Page

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+
+const AboutPage = () => {
+	return(
+		<div>
+			<h1>About Page</h1>
+			<p>Well this is akward</p>
+		</div>
+			
+		)
+}
+
+export default AboutPage
